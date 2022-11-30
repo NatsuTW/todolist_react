7fd0364ed7fe348c2be622aa2e91dd737cd4375c
Natsu <natsu.tw0919@gmail.com>
2022-11-30 09:30:54 +0900

    feat: add authinput props at login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
   AuthContainer,
   AuthInputContainer,
@@ -8,6 +9,9 @@ import { ACLogoIcon } from 'assets/images';
 import { AuthInput } from 'components';
 
 const LoginPage = () => {
+  const {username, setUsername} = useState('')
+  const {password, setPassword} = useState('')
+
   return (
     <AuthContainer>
       <div>
@@ -16,11 +20,22 @@ const LoginPage = () => {
       <h1>登入 Todo</h1>
 
       <AuthInputContainer>
-        <AuthInput />
+        <AuthInput
+          label="帳號"
+          placeholder="請輸入帳號"
+          value={username}
+          onChange={(nameInput) => setUsername(nameInput)}
+        />
       </AuthInputContainer>
 
       <AuthInputContainer>
-        <AuthInput />
+        <AuthInput
+          type="password"
+          label="密碼"
+          placeholder="請輸入密碼"
+          value={password}
+          onChange={(passwordInput) => setPassword(passwordInput)}
+        />
       </AuthInputContainer>
       <AuthButton>登入</AuthButton>
       <AuthLinkText>註冊</AuthLinkText>
