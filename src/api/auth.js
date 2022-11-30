1c71380a3540a52b09c54c2836b18c22fbd66fbe
Natsu <natsu.tw0919@gmail.com>
2022-11-30 11:11:58 +0900

    feat: add login api function

---
+++ src/api/auth.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+
+const authUrl = 'https://todo-list.alphacamp.io/api/auth'
+
+export const login = async ({ username, password }) => {
+  try {
+    const { data } = await axios.post(`${authUrl}/login`, {
+      username,
+      password,
+    });
+
+    const { authToken } = data;
+
+    if (authToken) {
+      return { success: true, ...data };
+    }
+
+    return data;
+  } catch(error) {
+    console.error('[Login Failed]:', error)
+  }
+}
