1259e783f437b50f2ce694c03fa2aa1c494fdeac
Natsu <natsu.tw0919@gmail.com>
2022-11-30 22:04:01 +0900

    feat: add default auth context

---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,21 @@
+import { createContext, useState } from "react"
+
+const defaultAuthContext = {
+  isAuthenticated: false,
+  currentMember: null,
+  register: null,
+  login: null,
+  logout: null
+}
+
+const AuthContext = createContext(defaultAuthContext)
+
+const AuthProvider = ({children}) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const [payload, setPayload] = useState(null)
+
+  return <AuthContext.Provider>
+    value={{isAuthenticated, currentMember: payload}}
+  </AuthContext.Provider>
+
+}
