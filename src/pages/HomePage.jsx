c4706934faacd21c44c428b08f63ea9196a307f5
Natsu <natsu.tw0919@gmail.com>
2022-12-01 09:41:46 +0900

    feat: add home page navigation

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from 'contexts/AuthContext';
+
 const HomePage = () => {
+  const {isAuthenticated} = useAuth()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if(isAuthenticated) {
+      navigate('/todo')
+    } else {
+      navigate('/login')
+    }
+  },[navigate, isAuthenticated])
   return <div>HomePage</div>;
 };
 
